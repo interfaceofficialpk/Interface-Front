d882d3ac2e7a711fe8df3af8aa84b02afa469582
Suleman275 <m.suleman275@gmail.com>
2025-10-21 20:25:08 +0500

    downgrade vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify",
-    },
-  },
   plugins: [react(), tailwindcss()],
 });
